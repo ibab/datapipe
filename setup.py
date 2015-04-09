979c53ab6e225b52de58ce06edef6756c0519653
Igor Babuschkin <igor@babuschk.in>
2015-04-09 18:28:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name='datapipe',
+      version='0.0.0',
+      description='Flexible data processing framework',
+      url='http://github.com/ibab/datapipe',
+      author='Igor Babuschkin',
+      author_email='igor@babuschk.in',
+      license='MIT',
+      packages=['datapipe'],
+      install_requires=[
+          'six',
+          'dask',
+      ],
+      zip_safe=False)
