a85270fa381ba8972b82038bed149d38e907a1b3
Igor Babuschkin <igor@babuschk.in>
2015-04-09 18:12:24 +0200

    Get basic dependency resolution working
    The entire dependency resolution is outsourced to dask for now.
    
    This means we only have to build a dictionary with one entry
    per target, and dask will figure out in which order we need to
    run the tasks.
    
    We still need to incorporate exists() and newer() to make sure that
    only necessary tasks are executed.

--- datapipe/target.py
+++ datapipe/target.py
@@ -1,4 +1,9 @@
-from .task import get_current_task
+import collections
+from operator import getitem
+import functools
+import dask
+
+from .task import Task, get_current_task
 
 class Target(object):
     def __init__(self):
@@ -18,6 +23,12 @@ class Target(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(' + repr(self.get()) + ')'
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
 
 class LocalFile(Target):
     def __init__(self, path):
@@ -39,4 +50,29 @@ class LocalFile(Target):
 
     def from_suffix(self, suf, app):
         return self.clone(path=self.path.replace(suf, app))
+
+def require(target):
+    d = {}
+
+    for t in Task.tasks:
+        inputs = t.inputs()
+        outputs = t.outputs()
+
+        def runner(t, *args):
+            t.run()
+        # Bind current task to runner
+        runner.__name__ = t.__class__.__name__
+        runner = functools.partial(runner, t)
+
+        if not isinstance(outputs, collections.Iterable):
+            outputs = (outputs,)
+            
+        for i, o in enumerate(outputs):
+            d[o] = (runner,) + tuple(inputs)
+
+        for inp in inputs:
+            if isinstance(inp, Target) and not inp.parent:
+                d[inp] = None
+    
+    dask.get(d, target)
 
--- datapipe/task.py
+++ datapipe/task.py
@@ -7,8 +7,9 @@ from .input import Input
 
 logger = logging.getLogger('datapipe')
 
-
 class Task:
+
+    tasks = []
 
     def __init__(self, *args, **kwargs):
         global current_task
@@ -26,21 +27,21 @@ class Task:
         self.user_run = self.run
 
         # Add custom modifications to output()
-        self.user_output = self.output
-        def output(self):
+        self.user_outputs = self.outputs
+        def outputs(self):
             global current_task
             current_task = self
-            outputs = self.user_output()
+            outputs = self.user_outputs()
             current_task = None
             return outputs
 
-        self.output = types.MethodType(output, self)
+        self.outputs = types.MethodType(outputs, self)
 
         # Add custom modifications to run()
         def run(self):
             logger.info('RUNNING {}'.format(self))
             self.user_run()
-            out = self.output()
+            out = self.outputs()
             if not isinstance(out, collections.Iterable):
                 out = [out]
             for o in out:
@@ -55,14 +56,19 @@ class Task:
 
         current_task = None
 
-    def __str__(self):
+        self.__class__.tasks.append(self)
+
+    def __repr__(self):
         result = []
         for k, w in self.input_values:
             result.append('{}={}'.format(k, repr(w)))
 
         return self.__class__.__name__ + '(' + ', '.join(result) + ')'
 
-    def output(self):
+    def inputs(self):
+        return self.input_args
+
+    def outputs(self):
         pass
 
     def run(self):
