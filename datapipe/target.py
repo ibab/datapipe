21a84990c26ecb823d6a5573c9b4de454e272718
Igor Babuschkin <igor@babuschk.in>
2015-04-10 13:32:17 +0200

    Implement timestamp-based skipping of targets
    This is a very rough prototype implementation of skipping
    up to date tasks. The timestamps of all targets are checked
    on require(). A task needs to be updated if any of its dependencies
    need to be updated, or if its timestamp is older than any of its
    dependencies.
    
    Ideally, there would be a database that would allow one to mix
    timestamps and hashes to determine up to date-ness.

--- datapipe/target.py
+++ datapipe/target.py
@@ -3,16 +3,17 @@ from operator import getitem
 import functools
 import dask
 import dask.threaded
+import os
+import logging
+logger = logging.getLogger('datapipe')
 
+from .history import History
 from .task import Task, get_current_task
 
 class Target(object):
     def __init__(self):
         self.parent = get_current_task()
-
-    def newer(self, targets):
-        # Check if this target is newer than the specified targets
-        pass
+        self.force_update = False
 
     def exists(self):
         # Check if this target exists
@@ -20,6 +21,9 @@ class Target(object):
 
     def get(self):
         # Get the underlying representation
+        pass
+
+    def timestamp_contents(self):
         pass
 
     def __repr__(self):
@@ -36,37 +40,65 @@ class LocalFile(Target):
         super(LocalFile, self).__init__()
         self.path = path
 
-    def clone(self, path=None):
-        if path is None:
-            return LocalFile(self.path)
-        else:
-            return LocalFile(path)
+    def timestamp_contents(self):
+        return os.path.getmtime(self.path)
 
     def exists(self):
-        import os
         return os.path.exists(self.path)
 
     def get(self):
         return self.path
 
-    def from_suffix(self, suf, app):
-        return self.clone(path=self.path.replace(suf, app))
+def is_uptodate(target):
+    # TODO make this more efficient by calculating the status for all targets once (in require?)
 
-def require(target, workers=1):
+    if target.force_update:
+        return False
+
+    if not target.exists():
+        return False
+
+    if target.parent:
+        inputs = filter(lambda o: isinstance(o, Target), target.parent.inputs())
+        upstream_uptodate = all(map(is_uptodate, inputs))
+        upstream_timestamps = map(lambda o: o.timestamp_contents(), filter(lambda o: o.exists(), inputs))
+        if not upstream_uptodate:
+            return False
+        elif upstream_timestamps and target.timestamp_contents() < max(upstream_timestamps):
+            return False
+
+    return True
+
+def require(target, workers=1, update_from=None):
+
+
+    if update_from:
+        update_from.force_update = True
+        logger.info('REQUIRE {} UPDATE FROM {}'.format(target, update_from))
+    else:
+        logger.info('REQUIRE {}'.format(target))
+
     d = {}
 
     for t in Task.tasks:
         inputs = t.inputs()
         outputs = t.outputs()
 
-        def runner(t, *args):
-            t.run()
+        if not isinstance(outputs, collections.Iterable):
+            outputs = (outputs,)
+
+        if all(map(lambda o: is_uptodate(o), filter(lambda o: isinstance(o, Target), outputs))):
+            # We can skip this task
+            def runner(t, *args):
+                logger.info('SKIPPING {}'.format(t))
+        else:
+            # This task needs to be executed
+            def runner(t, *args):
+                t.run()
+
         # Bind current task to runner
         runner.__name__ = t.__class__.__name__
         runner = functools.partial(runner, t)
-
-        if not isinstance(outputs, collections.Iterable):
-            outputs = (outputs,)
 
         for i, o in enumerate(outputs):
             d[o] = (runner,) + inputs
